c1adea4f6be6ae01ef3616ed087e97beb0d8c0bb
Angelo Antonio Scholze <antonioscholze@gmail.com>
2024-06-15 21:38:09 -0300

    added base for the pages and router reference

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,4 +1,6 @@
 import 'package:go_router/go_router.dart';
+import 'package:todo_list_basic/features/list/presentation/pages/list_page.dart';
+import 'package:todo_list_basic/features/settings/presentation/pages/settings_page.dart';
 
 import '../../features/home/presentation/pages/home_page.dart';
 import '../../features/login/presentation/pages/login_page.dart';
@@ -11,13 +13,24 @@ final GoRouter router = GoRouter(
       builder: (context, state) => const LoginPage(),
     ),
     GoRoute(
-        path: '/home',
-        builder: (context, state) => const HomePage(),
-        routes: [
-          GoRoute(
-            path: 'item',
-            builder: (context, state) => const ItemPage(),
-          ),
-        ]),
+      path: '/home',
+      builder: (context, state) => const HomePage(),
+      routes: [
+        GoRoute(
+          path: 'list',
+          builder: (context, state) => const ListPage(),
+          routes: [
+            GoRoute(
+              path: 'item',
+              builder: (context, state) => const ItemPage(),
+            ),
+          ],
+        ),
+        GoRoute(
+          path: 'settings',
+          builder: (context, state) => const SettingsPage(),
+        )
+      ],
+    ),
   ],
 );
