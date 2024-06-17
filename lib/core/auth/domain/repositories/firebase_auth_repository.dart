f900c33ac948513f50434d463ba19647943528c4
Angelo Antonio Scholze <antonioscholze@gmail.com>
2024-06-16 22:05:14 -0300

    implemented a contract for auth repositories and made a firebase auth repositorie to handle operations

---
+++ lib/core/auth/domain/repositories/firebase_auth_repository.dart
@@ -0,0 +1,27 @@
+import 'package:todo_list_basic/core/auth/domain/repositories/auth_repository.dart';
+
+class FirebaseAuthRepository implements AuthRepository {
+  @override
+  Future<bool> logIn() {
+    // TODO: implement logIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> signIn() {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> deleteAccount() {
+    // TODO: implement deleteAccount
+    throw UnimplementedError();
+  }
+}
