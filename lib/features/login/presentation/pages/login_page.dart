627cbfcd3ef39dd84219f788c7e5d0fda6ae38a2
Angelo Antonio Scholze <antonioscholze@gmail.com>
2024-06-17 23:35:27 -0300

    added svg package, added drawer icon svg, initial layout code for login page

--- lib/features/login/presentation/pages/login_page.dart
+++ lib/features/login/presentation/pages/login_page.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Teste'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 20,
+            right: 20,
+            child: IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset('assets/icons/drawer_bars.svg'),
+            ),
+          ),
+        ],
       ),
     );
   }
